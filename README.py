debcae0a2eb993c7a2e5146b1d7249aff08c9542
Wahaj Al Obid <whalobid@student.rtc.edu>
2024-10-24 17:48:50 -0700

    Create README.py

---
+++ README.py
@@ -0,0 +1,36 @@
+# Simple Calculator 
+
+This Python script allows the user to input two numbers and an operator and then returns the result of the chosen operation.
+
+## Getting Started
+
+These instructions will guide you through setting up and running your first Python script on your local machine, which you will use to experiment as part of your CNE330 class.
+
+### Prerequisites
+
+This project requires Python 3.13 to run. To Download it:
+Go to the official Python website: https://www.python.org/downloads/release/python-3130/.
+
+Also, recommended to use PyCharm as your Integrated Development Environment (IDE) for Python development.
+
+
+Download and Install PyCharm:
+Go to the official JetBrains website: https://www.jetbrains.com/pycharm/download/.
+Download the appropriate version for your operating system (Community Edition is free).
+
+## Running
+
+Once you have installed the required dependencies and set up your environment, you can run the program using Python 3.13 or PyCharm by copying and pasting the script into the Python interpreter or PyCharm editor.
+
+When you enter run, you must input the 1st and 2nd numbers and choose the required math operation to get the result.
+
+
+## Thanks
+I want to thank the following:
+
+I used my instructor's demo video to guide me through the assignment. 
+
+I also used some YouTube videos, such as https://www.youtube.com/watch?v=-0GjKG4gRmY. 
+
+I Also used ChatGPT to understand how to write the running part.
+
